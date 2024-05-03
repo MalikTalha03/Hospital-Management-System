90d43f24d3de37477afe73e45388186fa2411975
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2024-05-03 11:15:50 +0500

    Refactor PATCH and GET routes in patient API

--- src/app/api/patients/[id]/route.js
+++ src/app/api/patients/[id]/route.js
@@ -1,26 +1,36 @@
 import { Patient } from "@/models/patient";
 import { connectToDatabase } from "@/utils/db";
 
-export const PATCH = async(req,{params}) => {
+export const PATCH = async (req, { params }) => {
   await connectToDatabase();
   try {
-    const { id } = params.id;
+    const { id } = params;
     const data = await req.json();
-    const patient = await Patient.findByIdAndUpdate(id, data , { new: true });
+    const patient = await Patient.findByIdAndUpdate(id, data, { new: true });
     return new Response(JSON.stringify(patient), { status: 200 });
   } catch (error) {
-    new Response(error.message, { status: 500 });
+    return new Response(error.message, { status: 500 });
   }
-}
+};
 
-export const DELETE = async (req,{params}) => {
+export const GET = async (req, { params }) => {
   await connectToDatabase();
   try {
-    const { id } = params.id;
+    const { id } = params;
+    const patient = await Patient.findById(id);
+    return new Response(JSON.stringify(patient), { status: 200 });
+  } catch (error) {
+    return new Response(error.message, { status: 500 });
+  }
+};
+
+export const DELETE = async (req, { params }) => {
+  await connectToDatabase();
+  try {
+    const { id } = params;
     await Patient.findByIdAndDelete(id);
     return new Response("Patient deleted successfully", { status: 200 });
   } catch (error) {
-    new Response(error.message, { status: 500 });
+    return new Response(error.message, { status: 500 });
   }
-}
-
+};
