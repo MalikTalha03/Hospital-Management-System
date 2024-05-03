e2609251198cbdf18d857a3e2a666edfd963ebd9
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2024-05-03 11:16:31 +0500

    Refactor patient page component and import statements, and add session handling

--- src/app/patients/page.jsx
+++ src/app/patients/page.jsx
@@ -1,17 +1,42 @@
-'use client';
-import { useState } from 'react';
-import Sidebar from '@/components/Sidebar'; 
-import PatientsTable from '@/components/PatientTable';
+"use client";
+import { useState, useEffect } from "react";
+import { useSession, signIn } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import Sidebar from "@/components/Sidebar";
+import PatientsTable from "@/components/PatientTable";
+import Loader from "@/components/Loader";
 
 export default function MainComponent() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      // Show alert and redirect after 3 seconds
+      alert(
+        "Please log in to view this page. You will be redirected to the login page."
+      );
+      setTimeout(() => {
+        router.push("/login");
+      }, 500);
+    }
+  }, [status, router]);
+
+  if (status === "loading") {
+    return <Loader />;
+  }
+
+  if (!session) {
+    return null; // The alert and redirection is handled in useEffect
+  }
 
   return (
     <div className="flex h-screen bg-gray-100">
       <button
         className="p-4 text-gray-600 focus:outline-none md:hidden"
         onClick={() => setSidebarOpen(!sidebarOpen)}
-      >     
+      >
         <svg
           className="w-6 h-6"
           fill="none"
@@ -25,22 +50,19 @@ export default function MainComponent() {
         </svg>
       </button>
 
-      {/* Sidebar */}
-      <div className={`fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 ease-out transform translate-x-0 bg-white p-5 ${
-          sidebarOpen ? 'translate-x-0' : '-translate-x-full'
+      <div
+        className={`fixed inset-y-0 left-0 z-30 w-64 overflow-y-auto transition duration-300 ease-out transform translate-x-0 bg-white p-5 ${
+          sidebarOpen ? "translate-x-0" : "-translate-x-full"
         } md:relative md:translate-x-0`}
       >
         <Sidebar />
       </div>
 
-      {/* Content area */}
       <div className="flex-1 flex flex-col overflow-hidden">
-        {/* Header or any content you want to put before your table */}
         <header className="flex justify-between items-center p-4 shadow-md">
-          {/* Content of your header */}
+          {/* Optionally place logout or user info here */}
         </header>
 
-        {/* Main content */}
         <main className="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
           <div className="container mx-auto px-6 py-8">
             <PatientsTable />
