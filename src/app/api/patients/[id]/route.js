aa8913323281d306775fd7e3f5060c03a4cc40b3
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2024-05-05 00:14:25 +0500

    refactor: Update patient API PATCH route to use Firestore instead of MongoDB

--- src/app/api/patients/[id]/route.js
+++ src/app/api/patients/[id]/route.js
@@ -1,34 +1,40 @@
-import { Patient } from "@/models/patient";
-import { connectToDatabase } from "@/utils/db";
+import { db } from "@/utils/db";
 
 export const PATCH = async (req, { params }) => {
-  await connectToDatabase();
   try {
     const { id } = params;
     const data = await req.json();
-    const patient = await Patient.findByIdAndUpdate(id, data, { new: true });
-    return new Response(JSON.stringify(patient), { status: 200 });
+    const docRef = db.collection('patients').doc(id);
+
+    await docRef.update(data);
+    const updatedDoc = await docRef.get();
+
+    return new Response(JSON.stringify(updatedDoc.data()), { status: 200 });
   } catch (error) {
     return new Response(error.message, { status: 500 });
   }
 };
-
 export const GET = async (req, { params }) => {
-  await connectToDatabase();
   try {
     const { id } = params;
-    const patient = await Patient.findById(id);
-    return new Response(JSON.stringify(patient), { status: 200 });
+    const docRef = db.collection('patients').doc(id);
+    const doc = await docRef.get();
+
+    if (!doc.exists) {
+      return new Response("Patient not found", { status: 404 });
+    }
+
+    return new Response(JSON.stringify(doc.data()), { status: 200 });
   } catch (error) {
     return new Response(error.message, { status: 500 });
   }
 };
 
 export const DELETE = async (req, { params }) => {
-  await connectToDatabase();
   try {
     const { id } = params;
-    await Patient.findByIdAndDelete(id);
+    await db.collection('patients').doc(id).delete();
+
     return new Response("Patient deleted successfully", { status: 200 });
   } catch (error) {
     return new Response(error.message, { status: 500 });
