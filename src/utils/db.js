0cf8823acf7d9bac021de88b6699a66664614216
Muhammad Talha Naqshbandi <mtnaqshbandi2003@gmail.com>
2024-05-05 00:15:16 +0500

    refactor: Initialize Firebase Admin SDK and export Firestore and Storage instances

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,21 +1,20 @@
-import mongoose from "mongoose";
+// Import the Firebase Admin SDK
+import admin from 'firebase-admin';
 
-let isConnected = false;
+// Check if the Firebase app has been initialized to avoid initializing it multiple times
+if (!admin.apps.length) {
+    // Parse the JSON string from the environment variable directly
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SDK_JSON);
 
-export const connectToDatabase = async () => {
-    mongoose.set('strictQuery', true);
-    if (isConnected) {
-        console.log('using existing connection');
-        return;
-    }
-    try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: 'hms',
-        });
-        isConnected = true;
-    }
-    catch (error) {
-        console.log('DB connection error:', error);
-    }
+    // Initialize Firebase Admin with the parsed service account credentials
+    admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount),
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    });
+
+    console.log('Firebase Admin initialized');
 }
 
+// Export Firestore and Storage instances for use in other parts of the application
+export const db = admin.firestore();
+export const storage = admin.storage();
